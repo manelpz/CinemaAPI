98affe71b2dd995b99063d4c7351a216a2a90996
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-04-25 14:46:56 -0500

    adding properties

---
+++ CinemaAPI/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CinemaAPI.Models
+{
+    public class Movie
+    {
+        public Movie()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Language { get; set; }
+
+    }
+}
