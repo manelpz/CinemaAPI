1a14a738356cb5aac7a160f2c4bc7e71a05b2204
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-04-27 13:58:18 -0500

    adding cinema db context

---
+++ CinemaAPI/Data/CinemaDBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaAPI.Data
+{
+    public class CinemaDBContext: DbContext
+    {
+        public CinemaDBContext()
+        {
+        }
+    }
+}
