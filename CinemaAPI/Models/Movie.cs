ab08e7bff07e3767fa21f9e911b5bee969f9295b
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-04-29 14:38:27 -0500

    Adding images property

--- CinemaAPI/Models/Movie.cs
+++ CinemaAPI/Models/Movie.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.AspNetCore.Http;
+
 namespace CinemaAPI.Models
 {
     public class Movie
@@ -11,6 +13,7 @@ namespace CinemaAPI.Models
         public string Name { get; set; }
         public string Language { get; set; }
         public double Rating { get; set; }
+        public IFormFile Image { get; set; }
 
     }
 }
