9f3491bbd89906ace7a97207d023aea71baff71c
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-05-02 18:08:08 -0500

    changing ImageURL

--- CinemaAPI/Models/Movie.cs
+++ CinemaAPI/Models/Movie.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Http;
 
@@ -12,13 +14,23 @@ namespace CinemaAPI.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Language { get; set; }
+        public DateTime PlayingDate { get; set; }
+        public DateTime PlayingTime { get; set; }
+        public double TicketPrice { get; set; }
         public double Rating { get; set; }
+        public string Genre { get; set; }
+        public string TrailorUrl { get; set; }
+        public string ImageUrl { get; set; }
+
 
         [NotMapped]
         public IFormFile Image { get; set; }
 
-        public string ImageUrl { get; set; }
+        public ICollection<Reservation> Reservations { get; set; }
+
+
 
     }
 }
---
+++ CinemaAPI/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CinemaAPI.Models
+{
+    public class User
+    {
+        public User()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public ICollection<Reservation> Reservations { get; set; }
+    }
+}
