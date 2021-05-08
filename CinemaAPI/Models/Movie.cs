f7ea856f95a9d58a23659890a2e2cbd5005a033b
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-05-07 23:21:58 -0500

    adding properties

--- CinemaAPI/Models/Movie.cs
+++ CinemaAPI/Models/Movie.cs
@@ -8,29 +8,17 @@ namespace CinemaAPI.Models
 {
     public class Movie
     {
-        public Movie()
-        {
-        }
 
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public string Language { get; set; }
-        public DateTime PlayingDate { get; set; }
-        public DateTime PlayingTime { get; set; }
-        public double TicketPrice { get; set; }
         public double Rating { get; set; }
-        public string Genre { get; set; }
-        public string TrailorUrl { get; set; }
-        public string ImageUrl { get; set; }
 
 
         [NotMapped]
         public IFormFile Image { get; set; }
 
-        public ICollection<Reservation> Reservations { get; set; }
-
-
+        
 
     }
 }
