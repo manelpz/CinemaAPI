c964421c6850c859403608f9b640141b90a46445
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-05-10 23:36:36 -0500

    adding dbset

--- CinemaAPI/Data/CinemaDBContext.cs
+++ CinemaAPI/Data/CinemaDBContext.cs
@@ -13,6 +13,8 @@ namespace CinemaAPI.Data
 
         public DbSet<Movie> Movies { get; set; }
 
-        
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Reservation> Reservations { get; set; }
     }
 }
