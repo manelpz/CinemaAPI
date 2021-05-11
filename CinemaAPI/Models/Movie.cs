d48fb669c6b828f8a4c7f28dd40559fe979cd375
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-05-10 23:30:58 -0500

    adding values movie

--- CinemaAPI/Models/Movie.cs
+++ CinemaAPI/Models/Movie.cs
@@ -11,9 +11,17 @@ namespace CinemaAPI.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Language { get; set; }
+        public string Duration { get; set; }
+        public DateTime PlayingDate { get; set; }
+        public DateTime PlayingTime { get; set; }
+        public double TicketPrice { get; set; }
         public double Rating { get; set; }
+        public string Genre { get; set; }
 
+        public string TrailorUrl { get; set; }
+        public string ImageUrl { get; set; }
 
         [NotMapped]
         public IFormFile Image { get; set; }
