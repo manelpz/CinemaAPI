a360480eb3d22041aeefd9b929c33e1ed69be1b9
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-05-10 23:23:34 -0500

    adding user class

---
+++ CinemaAPI/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CinemaAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        
+    }
+}
