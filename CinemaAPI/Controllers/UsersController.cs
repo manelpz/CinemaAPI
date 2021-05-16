71979db1fd745599de3ebd27f99070d06a0537b7
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-05-16 16:00:40 -0500

    adding security with jwt

--- CinemaAPI/Controllers/UsersController.cs
+++ CinemaAPI/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AuthenticationPlugin;
 using CinemaAPI.Data;
@@ -48,5 +50,42 @@ namespace CinemaAPI.Controllers
 
             return StatusCode(StatusCodes.Status201Created);
         }
+
+        [HttpPost]
+        public IActionResult Login([FromBody]User user) {
+            var userEmail = _dbContext.Users.FirstOrDefault(u => u.Email == user.Email);
+
+            if (userEmail == null) {
+
+                return NotFound();
+            }
+
+            if (!SecurePasswordHasherHelper.Verify(user.Password, userEmail.Password))
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]
+            {
+               new Claim(JwtRegisteredClaimNames.Email, user.Email),
+               new Claim(ClaimTypes.Email, user.Email),
+            };
+
+            var token = _auth.GenerateAccessToken(claims);
+
+            return new ObjectResult(new
+            {
+                access_token = token.AccessToken,
+                expires_in = token.ExpiresIn,
+                token_type = token.TokenType,
+                creation_Time = token.ValidFrom,
+                expiration_Time = token.ValidTo,
+                user_id = userEmail.Id
+            });
+
+
+        }
+
+
     }
 }
