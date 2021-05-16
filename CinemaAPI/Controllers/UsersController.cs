7881108b26a8dbe13a3ecc6bfef5241fc7647189
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-05-16 15:43:29 -0500

    adding libraries for jwt

--- CinemaAPI/Controllers/UsersController.cs
+++ CinemaAPI/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using CinemaAPI.Data;
 using CinemaAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,9 +18,13 @@ namespace CinemaAPI.Controllers
     public class UsersController : Controller
     {
         private CinemaDBContext _dbContext;
+        private IConfiguration _configuration;
+        private readonly AuthService _auth;
 
-        public UsersController(CinemaDBContext dbContext)
+        public UsersController(CinemaDBContext dbContext, IConfiguration configuration)
         {
+            _configuration = configuration;
+            _auth = new AuthService(_configuration);
             _dbContext = dbContext;
         }
 
