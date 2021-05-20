9c9049f6a059ab25af1442fcc7aabefe9818151a
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-05-19 23:35:48 -0500

    Update MoviesController.cs

--- CinemaAPI/Controllers/MoviesController.cs
+++ CinemaAPI/Controllers/MoviesController.cs
@@ -11,6 +11,8 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
+
+
 namespace CinemaAPI.Controllers
 {
     [Route("api/[controller]")]
@@ -44,6 +46,40 @@ namespace CinemaAPI.Controllers
 
             return StatusCode(StatusCodes.Status201Created);
         }
+
+        // PUT api/values/5
+        [Authorize(Roles ="admin")]
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromForm] Movie movieObj)
+        {
+            var movie = _dbContext.Movies.Find(id);
+
+            if (movie == null)
+            {
+                return NotFound("No record found against this id");
+            }
+            else
+            {
+                var guid = Guid.NewGuid();
+                var filePath = Path.Combine("wwwroot", guid + ".jpg");
+
+                if (movieObj.Image != null)
+                {
+
+                    var fileStream = new FileStream(filePath, FileMode.Create);
+                    movieObj.Image.CopyTo(fileStream);
+                    movie.ImageUrl = filePath.Remove(0, 7);
+
+                }
+
+                movie.Name = movieObj.Name;
+                movie.Language = movieObj.Language;
+                movie.Rating = movieObj.Rating;
+                _dbContext.SaveChanges();
+                return Ok("Record updated succefully");
+            }
+        }
+
 
 
     }
