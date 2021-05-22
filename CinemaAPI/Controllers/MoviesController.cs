6b58d6108955d1b9778163504f9eec45eefcb170
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-05-21 22:40:46 -0500

    Update MoviesController.cs

--- CinemaAPI/Controllers/MoviesController.cs
+++ CinemaAPI/Controllers/MoviesController.cs
@@ -11,8 +11,6 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-
-
 namespace CinemaAPI.Controllers
 {
     [Route("api/[controller]")]
